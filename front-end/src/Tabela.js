3db98246b899fd7e4ae171cfc068dcbf9c0837a4
Filipe Santos <143547860+Filipe-S-Silva@users.noreply.github.com>
2025-02-25 18:04:29 +0000

    salvar2

---
+++ front-end/src/Tabela.js
@@ -0,0 +1,29 @@
+function Tabela() {
+    return (
+        <table className='table'>
+
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Marca</th>
+                    <th>Modelo</th>
+                    <th>Ano</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+
+        </table>
+    )
+}
+
+export default Tabela;
