1dfd1d0b65cbcd855554c69737fe3e5a354aca01
Filipe Santos <143547860+Filipe-S-Silva@users.noreply.github.com>
2025-02-27 00:53:12 +0000

    reactOk

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -23,88 +23,123 @@ function App() {
       )
   }, [])
 
-  const digitando = (e) =>{
-    setObjVeiculo ({...objVeiculo, [e.target.name]:e.target.value})
+  const digitando = (e) => {
+    setObjVeiculo({ ...objVeiculo, [e.target.name]: e.target.value })
   }
 
   //cadastrar
-  const cadastrar = () =>{
+  const cadastrar = () => {
     //rota para cadastro
-    fetch('',{
+    fetch('', {
       method: "post",
-      body:JSON.stringify(objVeiculo),
-      headers:{
+      body: JSON.stringify(objVeiculo),
+      headers: {
         "Content-type": "application/json",
         "Accept": "application/json"
       }
     })
-    .then(retorno => retorno.json())
-    .then(retornoConvertido =>{
-      if(retornoConvertido.mensagem !== undefined){
-        alert(retornoConvertido.mensagem)
-      } else {
-        setVeiculos([...veiculos, retornoConvertido]);
-        alert('Produto cadastrado com sucesso')
-        limparFormulario()
+      .then(retorno => retorno.json())
+      .then(retornoConvertido => {
+        if (retornoConvertido.mensagem !== undefined) {
+          alert(retornoConvertido.mensagem)
+        } else {
+          setVeiculos([...veiculos, retornoConvertido]);
+          alert('Produto cadastrado com sucesso')
+          limparFormulario()
 
-      }
+        }
 
-    })
+      })
   }
 
   //limpar formulario
-  const limparFormulario = ()=>{
+  const limparFormulario = () => {
     setObjVeiculo(veiculo);
     setBtnCadastrar(true)
   }
 
   //selecionar
-  const selecionarVeiculo = (indice) =>{
+  const selecionarVeiculo = (indice) => {
     setObjVeiculo(veiculos[indice])
     setBtnCadastrar(false)
   }
 
   //remover
-  const remover = () =>{
+  const remover = () => {
     //rota para de remoção
-    fetch(''+ objVeiculo.codigo,{
+    fetch('' + objVeiculo.codigo, {
       method: "delete",
 
-      headers:{
+      headers: {
         "Content-type": "application/json",
         "Accept": "application/json"
       }
     })
-    .then(retorno => retorno.json())
-    .then(retornoConvertido => {
+      .then(retorno => retorno.json())
+      .then(retornoConvertido => {
 
-      //Mesagem do retorno
-      alert(retornoConvertido.mensagem);
+        //Mesagem do retorno
+        alert(retornoConvertido.mensagem);
 
-      //vetor temporario 
-      let vetorTemporario = [...veiculos];
+        //vetor temporario 
+        let vetorTemporario = [...veiculos];
 
-      //indice
-      let indice = vetorTemporario.findIndex((p) =>{
-        return p.codigo === objVeiculo.codigo
+        //indice
+        let indice = vetorTemporario.findIndex((p) => {
+          return p.codigo === objVeiculo.codigo
+        })
+
+        //removendo veiculo do vetor
+        vetorTemporario.splice(indice, 1);
+
+        //atualizando o vetor de veiculos
+        setVeiculos(vetorTemporario);
+
+        //limpando formulario apos o processo
+        limparFormulario();
+
+
       })
-
-      //removendo veiculo do vetor
-      vetorTemporario.splice(indice, 1);
-    
-      //atualizando o vetor de protudos
-      setVeiculos(vetorTemporario);
-
-      //limpando formulario apos o processo
-      limparFormulario();
-
-
-    })
   }
 
   //alterar
-  const alterar = () =>{
-    
+  const alterar = () => {
+    //rota para alterar
+    fetch('', {
+      method: "put",
+      body: JSON.stringify(objVeiculo),
+      headers: {
+        "Content-type": "application/json",
+        "Accept": "application/json"
+      }
+    })
+      .then(retorno => retorno.json)
+      .then(retornoConvertido => {
+        if (retornoConvertido.mensagem !== undefined) {
+          alert(retornoConvertido.mensagem);
+        } else {
+
+          //mensagem 
+          alert("Veiculo alterado com sucesso")
+
+          //vetor temporario 
+          let vetorTemporario = [...produtos];
+
+          //indice
+          let indice = vetorTemporario.findIndex((p) => {
+            return p.codigo === objVeiculo.codigo;
+          })
+
+          //alterando veiculo do vetor
+          vetorTemporario[indice] = objVeiculo;
+
+          //atualizando o vetor de protudos
+          setVeiculos(vetorTemporario)
+
+          //limpando formulario apos o processo
+          limparFormulario();
+        }
+      })
   }
 
   return (
@@ -113,9 +148,9 @@ function App() {
 
       <p>{JSON.stringify(objVeiculo)}</p>
 
-      <Formulario botao = {btnCadastrar} eventoDoTeclado = {digitando} cadastrar = {cadastrar} obj = {objVeiculo} cancelar = {limparFormulario} remover = {remover}/>
+      <Formulario botao={btnCadastrar} eventoDoTeclado={digitando} cadastrar={cadastrar} obj={objVeiculo} cancelar={limparFormulario} remover={remover} alterar = {alterar}/>
 
-      <Tabela vetor={veiculos} selecionar = {selecionarVeiculo}/>
+      <Tabela vetor={veiculos} selecionar={selecionarVeiculo} />
 
     </div>
   );
